73571a95dd3a4965fa2a746a79ebad40f70cc8d3
Vladimir Obrizan <volodymyr.obrizan@gmail.com>
2022-04-01 19:15:30 +0300

    Fixed correct service.

--- test-selenium-2.py
+++ test-selenium-2.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.firefox.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+# from webdriver_manager.firefox import GeckoDriverManager
 
 # Открываем Chrome
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
